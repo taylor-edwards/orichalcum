2b7b9f4a710a19ac06e63e0df1d0d0175ffeac3f
Taylor Edwards <me@tayloredwards.net>
2021-07-03 12:45:06 -0700

    Force queued dispatch and allow multiple actions per dispatch

--- src/store.js
+++ src/store.js
@@ -1,10 +1,6 @@
-/**
- * Store API
- */
-
 const genID = (() => {
   let i = 0
-  return () => `id${(i++).toString(36).padStart(8, 0)}`
+  return () => `id:${(i++).toString(36).padStart(8, 0)}`
 })()
 
 export const createStore = (reducer, middleware, state) => {
@@ -12,70 +8,66 @@ export const createStore = (reducer, middleware, state) => {
   const listeners = {}
   let dispatchInProgress = false
 
-  const alertListeners = () =>
-    Object.values(listeners).forEach(listener => listener(state))
-
-  const queueForDispatch = laterAction => {
-    actionQueue.push(laterAction)
-    while (actionQueue.length > 0) {
-      if (!dispatchInProgress) {
-        dispatch(actionQueue.shift())
-      }
+  const queueForDispatch = (...actions) => {
+    actionQueue.push(...actions)
+    if (!dispatchInProgress) {
+      dispatchFromQueue()
     }
   }
 
-  const dispatch = (action, options = { silent: false }) => {
-    if (dispatchInProgress) {
-      queueForDispatch(action)
-      return
-    }
-
+  const dispatchFromQueue = () => {
     dispatchInProgress = true
+    while (typeof actionQueue[0] !== 'undefined') {
+      dispatch(actionQueue.shift())
+    }
+    dispatchInProgress = false
+    alertListeners()
+  }
 
+  const dispatch = action => {
     // middleware can alter the action but not state
     // middleware can dispatch additional actions
     // dispatch can be called asynchronously from middleware
     // use combineMiddleware to use more than one layer
-    action = middleware(action, state, queueForDispatch) ?? action
+    action = middleware(action, state, queueForDispatch)
 
     // reducers can alter state but not the action
     // use combineReducers to supply more than one reducer
     state = reducer(action, state)
-
-    if (!options.silent) {
-      alertListeners()
-    }
-
-    dispatchInProgress = false
   }
 
+  const attachListener = fn => {
+    const id = genID()
+    listeners[id] = fn
+    return () => {
+      delete listeners[id]
+    }
+  }
+
+  const alertListeners = () =>
+    Object.values(listeners).forEach(listener => listener(state))
+
   return {
-    dispatch,
+    dispatch: queueForDispatch,
     getState: () => state,
-    listen: fn => {
-      const id = genID()
-      listeners[id] = fn
-      return () => {
-        delete listeners[id]
-      }
-    },
+    listen: attachListener,
   }
 }
 
 export const combineReducers =
   (...reducers) =>
   (action, state) => {
-    for (let i = 0; i < reducers.length; i++) {
-      state = reducers[i](action, state)
+    for (const reducer in reducers) {
+      state = reducer(action, state)
     }
     return state
   }
 
 export const combineMiddleware =
-  (...middleware) =>
+  (...middlewares) =>
   (action, state, dispatch) => {
-    for (let i = 0; i < middleware.length; i++) {
-      action = middleware[i](action, state, dispatch) ?? action
+    for (const middleware in middlewares) {
+      action = middleware(action, state, dispatch)
     }
     return action
   }
